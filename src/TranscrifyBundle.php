1852d6d548f901eaf84549506902b20570b85ae7
Kordian555 <kordian.borowy2@gmail.com>
2026-01-06 11:54:44 +0100

    fix

--- src/TranscrifyBundle.php
+++ src/TranscrifyBundle.php
@@ -1,12 +1,13 @@
 <?php
 namespace Kordian\TranscrifyBundle;
 
-use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Symfony\Component\Config\FileLocator;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\Configurator\ContainerConfigurator;
+use Symfony\Component\HttpKernel\Bundle\AbstractBundle;
+use Symfony\Component\DependencyInjection\Loader;
 
-class TranscrifyBundle extends Bundle
+class TranscrifyBundle extends AbstractBundle
 {
-    public function test()
-    {
-        return 'hello';
-    }
+
 }
